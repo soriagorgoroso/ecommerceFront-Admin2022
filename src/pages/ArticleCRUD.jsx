63844530fe1a5786f7e77ce5cad50587c256f705
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-27 19:56:57 -0300

    Update ArticleCRUD.jsx

--- src/pages/ArticleCRUD.jsx
+++ src/pages/ArticleCRUD.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import NavBarAdmin from "../components/NavBarAdmin";
 import axios from "axios";
-import { Table } from "react-bootstrap";
+import { Table, Container } from "react-bootstrap";
 
 function ArticleCRUD() {
   const [articles, setArticles] = React.useState(null);
@@ -18,39 +18,44 @@ function ArticleCRUD() {
     articles && (
       <>
         <NavBarAdmin />
-        <h1>Articulos</h1>
-        <Table striped bordered hover size="sm">
-          <thead>
-            <tr>
-              <th>id</th>
-              <th>Nombre</th>
-              <th>Categoria</th>
-              <th>tamaño en cc</th>
-              <th>Stock</th>
-            </tr>
-          </thead>
-          <tbody>
-            {articles.map((article) => (
+        <Container>
+          <div className="d-flex align-items-center justify-content-between">
+            <h1>Articulos</h1>
+            <button className="btn btn-primary">Agregar articulo</button>
+          </div>
+          <Table striped bordered hover size="sm">
+            <thead>
               <tr>
-                <td>{article.id}</td>
-                <td>{article.name}</td>
-                <td>{article.category}</td>
-                <td>{article.sizecc}</td>
-                <td>{article.stock}</td>
-                <td>
-                  <a className="btn btn-success" rel="stylesheet" href="">
-                    Editar
-                  </a>
-                </td>
-                <td>
-                  <a className="btn btn-danger" rel="stylesheet" href="">
-                    Eliminar
-                  </a>
-                </td>
+                <th>id</th>
+                <th>Nombre</th>
+                <th>Categoria</th>
+                <th>tamaño en cc</th>
+                <th>Stock</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+              {articles.map((article) => (
+                <tr>
+                  <td>{article.id}</td>
+                  <td>{article.name}</td>
+                  <td>{article.category}</td>
+                  <td>{article.sizecc}</td>
+                  <td>{article.stock}</td>
+                  <td>
+                    <a className="btn btn-success" rel="stylesheet" href="">
+                      Editar
+                    </a>
+                  </td>
+                  <td>
+                    <a className="btn btn-danger" rel="stylesheet" href="">
+                      Eliminar
+                    </a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
       </>
     )
   );
