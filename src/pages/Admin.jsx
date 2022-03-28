48f7c7be3856a9d05ef40e702ea8b293c0d1a730
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-03-28 13:00:16 -0300

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import NavBarAdmin from "../components/NavBarAdmin";
 
 function Admin() {
   return (
     <>
       <NavBarAdmin />
-      <h1>Pagina del administrador</h1>
-      <p>Bienvenido a la pagina del administrador de Hack Beer</p>
+      <Container>
+        <h1>Pagina del administrador</h1>
+        <p>Bienvenido a la pagina del administrador de Hack Beer</p>
+      </Container>
     </>
   );
 }
