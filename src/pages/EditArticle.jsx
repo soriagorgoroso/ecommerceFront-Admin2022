090b656428208f1fb518e2208c86f15c4100f282
Fernando N. Marco <97688079+fnma88@users.noreply.github.com>
2022-04-01 11:13:24 -0300

    Update EditArticle.jsx

--- src/pages/EditArticle.jsx
+++ src/pages/EditArticle.jsx
@@ -4,10 +4,16 @@ import { Container } from "react-bootstrap";
 import NavBarAdmin from "../components/NavBarAdmin";
 import { useParams } from "react-router-dom";
 import ButtonEditArticle from "../components/ButtonEditArticle";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function EditArticle() {
+  const navigate = useNavigate();
+  const userLogged = useSelector((state) => state.user);
+  const [warning, setWarning] = React.useState(null);
   const params = useParams();
   const [article, setArticle] = React.useState(null);
+  const [editedArticle, setEditedArticle] = React.useState({});
 
   React.useEffect(() => {
     const getArticle = async () => {
@@ -19,13 +25,40 @@ function EditArticle() {
     getArticle();
   }, [params.id]);
 
+  const handleSubmit = async (ev) => {
+    ev.preventDefault();
+    for (const field in editedArticle) {
+      if (field === "") return;
+    }
+    const response = await axios(
+      {
+        method: "patch",
+        url: `${process.env.REACT_APP_API_URL}/articles/${article.id}`,
+        headers: {
+          Authorization: "Bearer " + userLogged.token,
+        },
+        data: editedArticle,
+      },
+      {
+        validateStatus: function (status) {
+          return status >= 200;
+        },
+      }
+    );
+    if (response.statusText === "OK") {
+      navigate("/articulos");
+    } else {
+      setWarning(response.data.msg);
+    }
+  };
+
   return (
     article && (
       <>
         <NavBarAdmin />
         <Container>
           <h1>Editar Articulo: {article.name}</h1>
-          <form action="" method="post" className="mb-5">
+          <form onSubmit={handleSubmit} className="mb-5">
             <label className="mt-3 w-75 form-label" htmlFor="name">
               Nombre
             </label>
@@ -34,22 +67,49 @@ function EditArticle() {
               className="w-75 form-control"
               id="name"
               type="text"
+              onChange={(ev) => {
+                setArticle({ ...article, name: ev.target.value });
+                setEditedArticle({ ...editedArticle, name: ev.target.value });
+              }}
             />
 
             <label className="mt-3 w-75 form-label" htmlFor="description">
               Descripción
             </label>
-            <textarea
+            <input
               defaultValue={article.description}
               className="w-75 form-control"
               id="description"
-              rows="4"
+              onChange={(ev) => {
+                setArticle({ ...article, description: ev.target.value });
+                setEditedArticle({
+                  ...editedArticle,
+                  description: ev.target.value,
+                });
+              }}
             />
 
+            {/* <label className="mt-3 w-75 form-label" htmlFor="image">
+              Imagen
+            </label>
+            <input className="w-75 form-control" id="image" type="file" /> */}
             <label className="mt-3 w-75 form-label" htmlFor="image">
               Imagen
             </label>
-            <input className="w-75 form-control" id="image" type="file" />
+            <input
+              placeholder="placeholder.jpg"
+              defaultValue={article.image}
+              className="w-75 form-control"
+              id="image"
+              type="text"
+              onChange={(ev) => {
+                setArticle({ ...article, image: ev.target.value });
+                setEditedArticle({
+                  ...editedArticle,
+                  image: ev.target.value,
+                });
+              }}
+            />
 
             <label className="mt-3 w-75 form-label" htmlFor="price">
               Precio
@@ -59,6 +119,13 @@ function EditArticle() {
               className="w-75 form-control"
               id="price"
               type="number"
+              onChange={(ev) => {
+                setArticle({ ...article, price: ev.target.value });
+                setEditedArticle({
+                  ...editedArticle,
+                  price: ev.target.value,
+                });
+              }}
             />
 
             <label className="mt-3 w-75 form-label" htmlFor="sicezz">
@@ -69,6 +136,13 @@ function EditArticle() {
               className="w-75 form-control"
               id="sicezz"
               type="number"
+              onChange={(ev) => {
+                setArticle({ ...article, sicezz: ev.target.value });
+                setEditedArticle({
+                  ...editedArticle,
+                  sicezz: ev.target.value,
+                });
+              }}
             />
 
             <label className="mt-3 w-75 form-label" htmlFor="stock">
@@ -79,6 +153,13 @@ function EditArticle() {
               className="w-75 form-control"
               id="stock"
               type="number"
+              onChange={(ev) => {
+                setArticle({ ...article, stock: ev.target.value });
+                setEditedArticle({
+                  ...editedArticle,
+                  stock: ev.target.value,
+                });
+              }}
             />
 
             <label className="mt-3 w-75 form-label" htmlFor="category">
@@ -89,6 +170,13 @@ function EditArticle() {
               className="w-75 form-control"
               id="category"
               type="text"
+              onChange={(ev) => {
+                setArticle({ ...article, category: ev.target.value });
+                setEditedArticle({
+                  ...editedArticle,
+                  category: ev.target.value,
+                });
+              }}
             />
 
             <label className="mt-3 w-75 form-label" htmlFor="topSeller">
@@ -97,8 +185,15 @@ function EditArticle() {
             <input
               defaultValue={article.topSeller}
               className="w-75 form-control"
-              id="category"
+              id="topSeller"
               type="text"
+              onChange={(ev) => {
+                setArticle({ ...article, topSeller: ev.target.value });
+                setEditedArticle({
+                  ...editedArticle,
+                  topSeller: ev.target.value,
+                });
+              }}
             />
 
             <label className="mt-3 w-75 form-label" htmlFor="ibus">
@@ -109,10 +204,19 @@ function EditArticle() {
               className="w-75 form-control"
               id="ibus"
               type="text"
+              onChange={(ev) => {
+                setArticle({ ...article, ibus: ev.target.value });
+                setEditedArticle({
+                  ...editedArticle,
+                  ibus: ev.target.value,
+                });
+              }}
             />
+            {warning && <p className="text-danger">{warning}</p>}
+            <button className="btn btn-success mt-3" type="submit">
+              Guardar cambios
+            </button>
           </form>
-          <ButtonEditArticle />
-          <br />
           <a className="my-3 btn btn-danger" href="/articulos">
             Ir atrás
           </a>
